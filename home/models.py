5e23d26e516ddf83d7d2fa1ca92657d3a8227ffe
Natalia Fijałkowska <natalkakoziel@gmail.com>
2022-11-28 21:28:48 +0100

    day 1

---
+++ home/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class ContactMessage(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.fullname}, {self.message[:10]}'
+
